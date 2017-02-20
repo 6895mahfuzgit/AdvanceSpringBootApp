bb4753b8aaaca9111deca8cbfac0729b788a1a06
6895mahfuzgit <6895mahfuz@gmail.com>
2017-02-20 23:42:05 +0600

    Added i18n

---
+++ src/main/java/com/AdvanceSpringBootApp/config/I18NConfig.java
@@ -0,0 +1,25 @@
+package com.AdvanceSpringBootApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+/**
+ * Created by user on 20-Feb-17.
+ */
+@Configuration
+public class I18NConfig {
+    /*
+    This ReloadableResourceBundleMessageSource
+     should be named as messageSource() on the
+      other hnd it gives an error*/
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+
+        ReloadableResourceBundleMessageSource reloadableResourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+        reloadableResourceBundleMessageSource.setBasename("classpath:i18n/messages");
+        reloadableResourceBundleMessageSource.setCacheMillis(1800); // for 30 minutes
+
+        return reloadableResourceBundleMessageSource;
+    }
+}
