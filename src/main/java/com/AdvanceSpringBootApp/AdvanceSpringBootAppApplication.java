8bdbb386e349dad7e545432dfccb49ad3cfe1c1b
6895mahfuzgit <6895mahfuz@gmail.com>
2017-02-20 19:36:29 +0600

    Initilized Project

---
+++ src/main/java/com/AdvanceSpringBootApp/AdvanceSpringBootAppApplication.java
@@ -0,0 +1,12 @@
+package com.AdvanceSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceSpringBootAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/AdvanceSpringBootApp/AdvanceSpringBootAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.AdvanceSpringBootApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvanceSpringBootAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
