52d9492548b47a0d542f3557977f409ed5e5eba7
6895mahfuzgit <6895mahfuz@gmail.com>
2017-02-21 22:40:23 +0600

    Added Login Page And it's Controller

---
+++ src/main/java/com/AdvanceSpringBootApp/web/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package com.AdvanceSpringBootApp.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by user on 21-Feb-17.
+ */
+@Controller
+public class LoginController {
+
+    public static final String LOGIN_VIEW_NAME = "user/login";
+
+    @RequestMapping("/login")
+    public String login() {
+        return LOGIN_VIEW_NAME;
+    }
+}
